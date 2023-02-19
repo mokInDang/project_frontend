98c91e31f595145602785afffc82631f320da4ec
DT\rangd <dropperin59@gmail.com>
2023-02-19 21:47:54 +0900

    style : #23 카카오 로그인 버튼 컴포넌트 수정
    styled.button -> styled.img
    사용자 정의 컴포넌트 이름은 대문자로 시작해야 함

--- project_frontend/src/components/login/kakaoLoginButton.js
+++ project_frontend/src/components/login/kakaoLoginButton.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
-import { kakao_login_large_narrow } from '../../assets/images';
+import {
+	kakao_login_large_wide,
+	kakao_login_large_narrow,
+	kakao_login_medium_narrow,
+	kakao_login_medium_wide,
+} from '../../assets/images';
 
-const StyledButton = styled.button`
-	background-image: url(${kakao_login_large_narrow});
-	background-repeat: no-repeat;
-	background-size: cover;
-	/* padding: -10px; */
-	color: transparent;
-	width: 300px;
-	height: 45px;
-	border: none;
+const LoginButtonImg = styled.img`
+	filter: drop-shadow(0px 0px 4px rgba(0, 0, 0, 0.13));
 `;
-
-function kakaoLoginButton() {
+function KakaoLoginButton() {
 	const REST_API_KEY = '60b35611c843f6c8f618a495ecc8eaf6';
 	const REDIRECT_URI = 'http://localhost:3000/api/member/join';
 	const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+	// const [innerWidth, setInnerWidth] = useState(document.body.offsetWidth);
+
+	// useEffect(()=>{
+	//     const resizeListner=()=>{
+	//         setInnerWidth(document.body.offsetWidth);
+	//     };
+	//     window.addEventListener("resize",resizeListner);
+	// })
 
 	return (
-		<a href={KAKAO_AUTH_URL}>
-			<StyledButton />
-		</a>
+		<div>
+			<a href={KAKAO_AUTH_URL}>
+				<LoginButtonImg src={kakao_login_large_narrow}></LoginButtonImg>
+			</a>
+		</div>
 	);
 }
 
-export default kakaoLoginButton;
+export default KakaoLoginButton;
