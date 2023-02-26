4b97bb2d360195aa690af5fa944aee423fa1cdd9
DT\rangd <dropperin59@gmail.com>
2023-02-27 00:58:31 +0900

    fix : #25 로그인 후 메인 화면으로 이동

--- project_frontend/src/pages/main.js
+++ project_frontend/src/pages/main.js
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 function Home() {
+	const KAKAO_CODE = new URL(document.location.href).searchParams.get('code');
+	console.log(KAKAO_CODE);
 	return (
 		<Fragment>
 			<div>메인 페이지_code formatting</div>
