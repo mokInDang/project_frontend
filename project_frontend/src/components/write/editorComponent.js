32a12d45921ef9c84af9ca8410bb11c97ac5ad00
Dropper-in <dropperin59@gmail.com>
2023-03-03 04:47:38 +0900

    feature : #31 editorComponent 수정, writeForm 컴포넌트 생성

--- project_frontend/src/components/write/editorComponent.js
+++ project_frontend/src/components/write/editorComponent.js
@@ -1,17 +1,58 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
-function EditorComponent() {
-	const [value, setValue] = useState('');
+function EditorComponent({ getHtmlContent }) {
+	const [content, setContent] = useState('');
+	const getContent =(e)=>{
+		setContent(e);
+		getHtmlContent(content);
+	}
+	const modules = {
+		toolbar: [
+			//[{ 'font': [] }],
+			[{ 'header': [1, 2, false] }],
+			['bold', 'italic', 'underline', 'strike'],
+			[
+				{ 'list': 'ordered' },
+				{ 'list': 'bullet' },
+				{ 'indent': '-1' },
+				{ 'indent': '+1' },
+			],
+			[{ 'align': [] }, { 'color': [] }, { 'background': [] }],
+		],
+	};
+
+	const formats = [
+		//'font',
+		'header',
+		'bold',
+		'italic',
+		'underline',
+		'strike',
+		'blockquote',
+		'list',
+		'bullet',
+		'indent',
+		'align',
+		'color',
+		'background',
+	];
+	useEffect(() => {
+		getContent()
+	},[]);
 
 	return (
-		<ReactQuill
-			theme="snow"
-			value={value}
-			onChange={setValue}
-            style={{width: '100%', height:'550px'}}
-		/>
+		<div style={{ height: '600px' }}>
+			<ReactQuill
+				style={{ height: '550px' }}
+				theme="snow"
+				modules={modules}
+				formats={formats}
+				value={content}
+				onChange={getContent}
+			/>
+		</div>
 	);
 }
 
