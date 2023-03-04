8d2cdd026ac3ff5634bc22dd17e8f9167e5635de
Dropper-in <dropperin59@gmail.com>
2023-03-04 16:40:28 +0900

    fix : #35 axios 요청 형태 변경 및 헤더의 Content-type 추가

--- project_frontend/src/pages/main.js
+++ project_frontend/src/pages/main.js
@@ -2,9 +2,9 @@ import React, { Fragment, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { Header } from '../components';
+import PropTypes from 'prop-types';
 
 const KAKAO_CODE = new URL(document.location.href).searchParams.get('code');
-
 function Home() {
 	window.history.pushState(KAKAO_CODE, '', `/`);
 	console.log(KAKAO_CODE);
@@ -13,25 +13,32 @@ function Home() {
 		alert('로그인을 축하드립니다.'); // 모달알림창구현해보기
 	}
 	useEffect(() => {
+		var today = new Date();
+		console.log(today);
+		let data = { authorizationCode: KAKAO_CODE };
 		const getAccessToken = async () => {
-			await axios({
-				url: `/api/member/join`, //서버 url로 수정하기
-				method: 'post',
-				headers: {
-					Authorization: `${KAKAO_CODE}`,
-				},
-			})
+			await axios
+				.post(
+					"http://15.165.153.213:8080/api/member/join",
+					JSON.stringify(data),
+					{
+						headers: { 'Content-Type': `application/json` },
+					}
+				)
 				.then((res) => {
 					console.log(KAKAO_CODE);
 					//인가코드를 보내고 나면
-					let accessToken = res.data.accessToken; //액세스 토큰 받아오기
-					let refreshToken = res.headers['refresh-token']; //리프레쉬 토큰 받아오기
-					localStorage.setItem('AccessToken', accessToken);
-					localStorage.setItem('Refresh_Token', refreshToken);
+					const jData = res.headers.get('Authorization');
+					// let accessToken = res.data.accessToken; //액세스 토큰 받아오기
+					// let refreshToken = res.headers['refresh-token']  ; //리프레쉬 토큰 받아오기
+					// localStorage.setItem('AccessToken', accessToken);
+					// localStorage.setItem('Refresh_Token', refreshToken);
+					localStorage.setItem('Token', jData);
 					console.log('토큰 받아오기 성공');
 				})
 				.catch((response) => {
 					console.log(response);
+					console.log('실패');
 				});
 		};
 		if (KAKAO_CODE) {
