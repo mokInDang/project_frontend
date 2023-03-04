1fe4bac7c60fdf399fd8e3a26709731f44d57f5d
Dropper-in <dropperin59@gmail.com>
2023-03-04 20:57:57 +0900

    fix : #35 Route path 및 redirect uri 수정

--- project_frontend/src/App.js
+++ project_frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />}/>
 				<Route path="/login" element={<Login />}/>
-				<Route path="/api/member/join/*" element={<Welcome />}/>
+				<Route path="/api/member/join" element={<Welcome />}/>
 				<Route path="/boards" element={<Write />}/>
 			</Routes>
 		</div>
