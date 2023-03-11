ce62f61b9206a9bd8063ca0cbe547acbec74d633
Dropper-in <dropperin59@gmail.com>
2023-03-12 04:03:48 +0900

    feat : #39 애플리케이션 마운트 시 onSilentRefresh 실행
    b2b140d, 9a9f8da

--- project_frontend/src/App.js
+++ project_frontend/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import { Home, Login, Welcome, Write } from './pages';
 import { Routes, Route } from 'react-router-dom';
-
+import axios from 'axios';
+import { onSilentRefresh } from './apis';
+import { useEffect } from 'react';
 function App() {
+	useEffect(() => {
+		onSilentRefresh();
+	});
+
 	return (
 		<div className="App">
 			<Routes>
