a65ddcf9b4a5f387d2e7abd8b3cd0964e298667f
Dropper-in <jangsuhyun0509@gmail.com>
2023-03-16 15:59:49 +0900

    fix : #51 onSilentRefresh 실행 시 onLoginSuccess 대신 setAuthHeader 실행

--- project_frontend/src/apis/login/login.js
+++ project_frontend/src/apis/login/login.js
@@ -11,7 +11,7 @@ const onLogin = async (res) => {
 				'Access-Control-Allow-Origin': `https://www.dongnejupging.xyz`,
 			},
 		})
-		.then(onLoginSuccess)
+		.then(onLoginSuccess(res))
 		.catch((error) => {
 			console.log(error);
 			console.log('onLogin 실패');
@@ -19,17 +19,19 @@ const onLogin = async (res) => {
 };
 let setAuthHeader = function (res) {
 	return new Promise((resolve) => {
-		console.log('onLoginSuccess 내 setAuthHeader 실행');
+		console.log('setAuthHeader 실행');
 		const Token = res.headers.get('Authorization');
 		axios.defaults.headers.common['Authorization'] = Token;
 		const { alias } = res.data;
-		resolve(console.log('onLoginSuccess 내 setAuthHeader 완료')); // resolve 함수 호출된 경우 비동기 처리 성공!
+		resolve(console.log('setAuthHeader 완료')); // resolve 함수 호출된 경우 비동기 처리 성공!
 	});
 };
 const onLoginSuccess = async (res) => {
+	console.log(res);
 	console.log('onLoginSuccess 실행');
 	await setAuthHeader(res)
-		.then(() => { // setHeader 성공 시 .then 안 함수 실행
+		.then(() => {
+			// setHeader 성공 시 .then 안 함수 실행
 			console.log('onLoginSuccess 내 onSilentRefresh 실행');
 			setTimeout(onSilentRefresh(), JWT_EXPIRY_TIME - 60000); // 액세스 토큰 만료 1분 전 재발급
 		})
@@ -43,7 +45,9 @@ const onSilentRefresh = () => {
 	console.log('onSilentRefresh 실행');
 	axios
 		.post('/api/auth/refresh')
-		.then(onLoginSuccess)
+		.then((res) => {
+			setAuthHeader(res);
+		})
 		.catch((error) => {
 			console.log(error);
 			console.log('onSilentRefresh 실패');
@@ -53,8 +57,9 @@ const reissueToken = () => {
 	const token = axios.defaults.headers.common.Authorization;
 	console.log(token);
 	// Todo : 로그아웃 시 Authorization undefined로 설정해줄 것
-	if (typeof token === 'string' && token.slice(0, 6) === 'Bearer') { 
+	if (typeof token === 'string' && token.slice(0, 6) === 'Bearer') {
 		onSilentRefresh();
-	} else console.log('type of axios default header Authorization is not string');
+	} else
+		console.log('type of axios default header Authorization is not string');
 };
 export { onLogin, onSilentRefresh, onLoginSuccess, reissueToken };
