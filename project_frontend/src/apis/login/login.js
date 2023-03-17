f6d767224f7ff81df69008421255480e12a1f3fb
Dropper-in <jangsuhyun0509@gmail.com>
2023-03-17 19:55:32 +0900

    test : #51 debugger문으로 디버그 테스트

--- project_frontend/src/apis/login/login.js
+++ project_frontend/src/apis/login/login.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const JWT_EXPIRY_TIME = 30 * 60 * 1000; // 만료 시간 (30분 밀리초로 표현)
 
 const onLogin = async (res) => {
-	console.log(`onLogin 실행`);
+	console.log(`1. onLogin 실행`);
 	await axios
 		.post('/api/auth/join', JSON.stringify(res), {
 			headers: {
@@ -11,7 +11,10 @@ const onLogin = async (res) => {
 				'Access-Control-Allow-Origin': `https://www.dongnejupging.xyz`,
 			},
 		})
-		.then(onLoginSuccess(res))
+		.then((res) => {
+			debugger;
+			onLoginSuccess(res);
+		})
 		.catch((error) => {
 			console.log(error);
 			console.log('onLogin 실패');
@@ -19,21 +22,27 @@ const onLogin = async (res) => {
 };
 let setAuthHeader = function (res) {
 	return new Promise((resolve) => {
-		console.log('setAuthHeader 실행');
+		console.log('3. setAuthHeader 실행');
 		const Token = res.headers.get('Authorization');
+		debugger;
 		axios.defaults.headers.common['Authorization'] = Token;
+		debugger;
 		const { alias } = res.data;
-		resolve(console.log('setAuthHeader 완료')); // resolve 함수 호출된 경우 비동기 처리 성공!
+		resolve(
+			console.log(`4. setAuthHeader 완료 ${axios.defaults.headers.common}`)
+		); // resolve 함수 호출된 경우 비동기 처리 성공!
 	});
 };
 const onLoginSuccess = async (res) => {
 	console.log(res);
-	console.log('onLoginSuccess 실행');
+	console.log('2. onLoginSuccess 실행');
 	await setAuthHeader(res)
 		.then(() => {
 			// setHeader 성공 시 .then 안 함수 실행
-			console.log('onLoginSuccess 내 onSilentRefresh 실행');
+			console.log('5. onLoginSuccess 내 onSilentRefresh 실행');
+			debugger;
 			setTimeout(onSilentRefresh(), JWT_EXPIRY_TIME - 60000); // 액세스 토큰 만료 1분 전 재발급
+			debugger;
 		})
 		.catch((error) => {
 			console.log(error);
@@ -42,7 +51,7 @@ const onLoginSuccess = async (res) => {
 };
 
 const onSilentRefresh = () => {
-	console.log('onSilentRefresh 실행');
+	console.log('6. onSilentRefresh 실행');
 	axios
 		.post('/api/auth/refresh')
 		.then((res) => {
