53dfd538c42443e5bf4a8fe9e46950e86be4f3f4
Dropper-in <dropperin59@gmail.com>
2023-03-20 15:54:15 +0900

    feat : #53 axios.delete 메서드로 로그아웃 기능 구현

--- project_frontend/src/apis/index.js
+++ project_frontend/src/apis/index.js
@@ -1,3 +1,4 @@
 import { onLogin, onLoginSuccess, onSilentRefresh, reissueToken } from './login/login';
+import { onLogout } from './logout';
 
-export { onLogin, onLoginSuccess, onSilentRefresh, reissueToken };
+export { onLogin, onLoginSuccess, onSilentRefresh, reissueToken, onLogout };
