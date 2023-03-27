8d87c044ba5daef88dd13de429400a31d84d3faa
Dropper-in <dropperin59@gmail.com>
2023-03-28 01:58:51 +0900

    feat : #61 서비스 로그아웃 후 카카오계정 로그아웃 구현

--- project_frontend/src/apis/logout.js
+++ project_frontend/src/apis/logout.js
@@ -1,35 +1,24 @@
 import axios from 'axios';
 
+const Host = window.location.host;
+const REST_API_KEY = '60b35611c843f6c8f618a495ecc8eaf6';
+const LOGOUT_REDIRECT_URI = `https://${Host}`;
+const KAKAO_LOGOUT_URL = `https://kauth.kakao.com/oauth/logout?client_id=${REST_API_KEY}&logout_redirect_uri=${LOGOUT_REDIRECT_URI}`;
+
 const onLogout = async () => {
 	console.log(`Logout 실행 : 카카오 계정과 함께 로그아웃`);
-	const token = axios.defaults.headers.common['Authorization'];
-	const Logout = async () => {
-		axios.defaults.headers.common['Authorization'] = token;
-		await axios
-			.delete('/api/auth/logout')
-			.then((res) => {
-				console.log(`동네줍깅 로그아웃 : Access token 삭제`);
-				axios.defaults.headers.common['Authorization'] = undefined;
-				console.log(res.status);
-			})
-			.catch((error) => {
-				console.log(error);
-				console.log(error.status);
-				console.log('동네줍깅 로그아웃 실패');
-			});
-	};
-	const Host = window.location.host;
-	const REST_API_KEY = '60b35611c843f6c8f618a495ecc8eaf6';
-	const LOGOUT_REDIRECT_URI = `https://${Host}`;
 	await axios
-		.get(
-			`https://kauth.kakao.com/oauth/logout?client_id=${REST_API_KEY}&logout_redirect_uri=${LOGOUT_REDIRECT_URI}`
-		)
-		.then(Logout)
+		.delete('/api/auth/logout')
+		.then((res) => {
+			console.log(`동네줍깅 로그아웃 : Access token 삭제`);
+			axios.defaults.headers.common['Authorization'] = undefined;
+			console.log(res.status);
+			window.location.replace(KAKAO_LOGOUT_URL);
+		})
 		.catch((error) => {
 			console.log(error);
 			console.log(error.status);
-			console.log('카카오계정과 함께 로그아웃 실패');
+			console.log('동네줍깅 로그아웃 실패');
 		});
 };
 
