a9895b2c017fe6834277efbe0bc027c9734c4ccb
Dropper-in <dropperin59@gmail.com>
2023-04-26 06:35:29 +0900

    fix : #101 인증 필요 페이지 접근 시 우선적으로 alert 창 뜨는 문제 해결
    if 문으로 분기하여 accessToken이 없는 else의 경우 alert 하도록 수정

--- project_frontend/src/utils/privateRoutes.js
+++ project_frontend/src/utils/privateRoutes.js
@@ -7,13 +7,15 @@ const PrivateRoutes = ({ authentication }) => {
 		//인증이 반드시 필요한 페이지. 미인증 시 로그인 페이지로, 인증을 한 상태일 경우 해당 페이지로 이동
 		// if (userInfo !== null && userInfo.region === 'DEFAULT_REGION') {
 		// }
-		alert('로그인 후 이용하실 수 있습니다.');
-		return secureLocalStorage.getItem('accessToken') !== null &&
-			secureLocalStorage.getItem('accessToken').slice(0, 6) === 'Bearer' ? (
-			<Outlet />
-		) : (
-			<Navigate to="/login" />
-		);
+		if (
+			secureLocalStorage.getItem('accessToken') !== null &&
+			secureLocalStorage.getItem('accessToken').slice(0, 6) === 'Bearer'
+		) {
+			return <Outlet />;
+		} else {
+			alert('로그인 후 이용하실 수 있습니다.');
+			return <Navigate to="/login" />;
+		}
 	} else {
 		// 반드시 미인증 상태여야 하는 페이지. 미인증 시 해당 페이지로, 인증을 한 상태일 경우 메인 페이지로 이동
 		return secureLocalStorage.getItem('accessToken') !== null &&
