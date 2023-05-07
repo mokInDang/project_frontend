6adaeec078040a8b4892d342ecaf1831d2d9d40a
Dropper-in <dropperin59@gmail.com>
2023-05-07 20:47:29 +0900

    refactor : #132 메인 페이지 무한 스크롤 컴포넌트 분리

---
+++ project_frontend/src/components/main/recruitmentInfiniteScroll.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { ImFileEmpty, ImFileText2 } from 'react-icons/im';
+import { isLogined } from '../../utils';
+import { InfiniteScroll } from './infiniteScroll';
+import { TabWrapper, TabDiv } from '../../components';
+import { useNavigate } from 'react-router';
+const RecruitmentInfiniteScroll = () => {
+	const [regionTab, setRegionTap] = useState(0);
+	const navigate = useNavigate();
+	return (
+		<>
+			<TabWrapper regionTab={regionTab}>
+				<TabDiv
+					onClick={(e) => {
+						e.preventDefault();
+						setRegionTap(0);
+					}}
+					className="entireRegion">
+					<ImFileEmpty
+						size="2.5rem"
+						style={{ marginRight: '1rem' }}
+					/>
+					전체
+				</TabDiv>
+				<TabDiv
+					onClick={(e) => {
+						e.preventDefault();
+						isLogined(setRegionTap, 1, navigate);
+					}}
+					className="myRegion">
+					<ImFileText2
+						size="2.5rem"
+						style={{ marginRight: '1rem' }}
+					/>
+					내 지역
+				</TabDiv>
+			</TabWrapper>
+			<InfiniteScroll regionTab={regionTab} />
+		</>
+	);
+};
+export { RecruitmentInfiniteScroll };
