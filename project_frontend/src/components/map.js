076bf2d9082149133fe71afaee59f8587694fa00
Dropper-in <dropperin59@gmail.com>
2023-05-12 03:49:27 +0900

    feat : #141 마커 클릭 시 커스텀 오버레이 표시, 커스텀 오버레이 클릭 시 주소 및 좌표 state에 저장

--- project_frontend/src/components/map.js
+++ project_frontend/src/components/map.js
@@ -4,32 +4,43 @@ import { useLocation } from 'react-router';
 import styled from 'styled-components';
 
 const MapWrapper = styled.div`
-	width: 75rem;
+	width: 70rem;
 	height: 40rem;
-	padding: 1.5rem;
+	box-sizing: border-box;
 	border: 0.1rem solid #ccc;
 	font-size: 1.5rem;
+	padding: 1.5rem;
 	font-family: NanumSquareNeo;
 	border-radius: 0.5rem;
 	background-color: #fff;
-
+	@media (max-width: 778px) {
+		width: 95%;
+		height: 30rem;
+	}
+	#map {
+		display: flex;
+		width: 100%;
+		height: 100%;
+		box-sizing: border-box;
+	}
 	.customoverlay {
 		position: relative;
 		background: #fff;
 		position: relative;
 		border-radius: 1rem;
 		opacity: 0.9;
+		border: 1px solid #aaa;
 
 		.title {
 			display: block;
 			text-align: center;
 			background: #fff;
-			padding: 0.5rem 1rem;
+			padding: 0.7rem;
 			font-size: 1.5rem;
 			font-weight: bold;
 			border-radius: 1rem;
 		}
-		.selected {
+		span {
 			display: block;
 			text-align: center;
 			background: rgba(87, 200, 77, 1);
@@ -38,21 +49,30 @@ const MapWrapper = styled.div`
 			font-weight: bold;
 			border-radius: 0 0 1rem 1rem;
 		}
+		.selected {
+			background: red;
+		}
 		:after {
+			display: block;
 			content: '';
 			position: absolute;
-			margin-left: -0.8rem;
+			top: 50%;
 			left: 50%;
-			top: 100%;
-			border: 0.8rem solid transparent;
-			border-top: 0.8rem solid rgba(87, 200, 77, 1);
+			transform: translate(-100%, -50%);
+			top: 0.5rem;
+			rotate: 45deg;
+			width: 1rem;
+			height: 1rem;
+			background-color: white;
+			border-left: 1px solid #aaa;
+			border-top: 1px solid #aaa;
 		}
 	}
 `;
 const Map = ({ keyword }) => {
 	const [address, setAddress] = useState('');
-	const [x, setX] = useState(0);
-	const [y, setY] = useState(0);
+	const [x, setX] = useState();
+	const [y, setY] = useState();
 	var clickedOverlay = null;
 
 	useEffect(() => {
@@ -63,10 +83,14 @@ const Map = ({ keyword }) => {
 				level: 3, // 지도의 확대 레벨
 			};
 		var map = new kakao.maps.Map(mapContainer, mapOption);
+		var selectedMarker = null;
+
 		// 장소 검색 객체를 생성합니다
 		var ps = new kakao.maps.services.Places();
+
 		// 키워드로 장소를 검색합니다
 		ps.keywordSearch('숭실대학교', placesSearchCB);
+
 		function placesSearchCB(data, status) {
 			if (status === kakao.maps.services.Status.OK) {
 				// 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
@@ -92,51 +116,75 @@ const Map = ({ keyword }) => {
 		// 지도에 마커를 표시하는 함수입니다
 		function DisplayMarker(place) {
 			const markerPosition = new kakao.maps.LatLng(place.y, place.x);
-			// 마커를 생성하고 지도에 표시합니다
+
+			var normalImage = new kakao.maps.MarkerImage(
+				'https://t1.daumcdn.net/mapjsapi/images/marker.png',
+				new kakao.maps.Size(25, 35),
+				new kakao.maps.Point(13, 34)
+			);
+			var clickImage = new kakao.maps.MarkerImage(
+				'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
+				new kakao.maps.Size(31, 35),
+				new kakao.maps.Point(13, 34)
+			);
+
+			// 마커를 생성하고 지도에 표시합니다. 이미지는 기본 마커 이미지를 사용합니다
 			var marker = new kakao.maps.Marker({
 				map: map,
 				position: markerPosition,
+				image: normalImage,
 			});
-			// 커스텀 오버레이를 생성하고 지도에 표시한다
-			var customOverlay = new kakao.maps.CustomOverlay({
-				clickable: true, // 커스텀 오버레이 클릭 시 지도에 이벤트를 전파하지 않도록 설정한다
-				content:
-					'<div class=customoverlay id=customoverlay>' +
-					// JSON.stringify(place) +
-					'<span class=title>' +
-					place.place_name +
-					'</span>' +
-					'<span class=selected>' +
-					'선택됨' +
-					'</span>' +
-					'</div>',
-				position: markerPosition, // 커스텀 오버레이를 표시할 좌표
-				xAnchor: 0.5, // 컨텐츠의 x 위치
-				yAnchor: 2.1, // 컨텐츠의 y 위치
-			});
-			customOverlay.setMap(null);
-			// 마커에 클릭이벤트를 등록합니다
+
 			kakao.maps.event.addListener(marker, 'click', function () {
 				map.panTo(markerPosition);
 				if (clickedOverlay) {
 					clickedOverlay.setMap(null);
-					// $('#customoverlay').parent('div').css('z-index', '1');
 				}
+				// 커스텀 오버레이를 생성하고 지도에 표시
+				var content = document.createElement('div');
+				var title = document.createElement('span');
+				var selectSpan = document.createElement('span');
+				var textNode = document.createTextNode('선택하기');
+				content.className = 'customoverlay';
+				title.className = 'title';
+				title.appendChild(document.createTextNode(place.place_name));
+				content.appendChild(title);
+				selectSpan.appendChild(textNode);
+				content.appendChild(selectSpan);
+				content.onclick = function () {
+					selectSpan.className = 'selected';
+					var selectedTextNode = document.createTextNode('선택됨');
+					textNode.replaceWith(selectedTextNode);
+					const address = place.road_address_name
+						? place.road_address_name + ' ' + place.place_name
+						: place.address_name + ' ' + place.place_name;
+					setY(place.y);
+					setX(place.x);
+					setAddress(address);
+				};
+
+				var customOverlay = new kakao.maps.CustomOverlay({
+					clickable: true, // 커스텀 오버레이 클릭 시 지도에 이벤트를 전파하지 않도록 설정
+					content: content,
+					position: markerPosition, // 커스텀 오버레이를 표시할 좌표
+					xAnchor: 0.5, // 컨텐츠의 x 위치
+					yAnchor: -0.3, // 컨텐츠의 y 위치
+				});
+
+				customOverlay.setMap(null);
+				// 마커에 클릭이벤트를 등록합니다
 				customOverlay.setMap(map);
-				var markerImage = new kakao.maps.MarkerImage(
-					'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
-					new kakao.maps.Size(31, 35),
-					new kakao.maps.Point(13, 34)
-				);
 				// marker.setImage(markerImage);
-				// $('#customverlay').parent('div').css('z-index', '100');
+				if (!selectedMarker || selectedMarker !== marker) {
+					// 클릭된 마커 객체가 null이 아니면
+					// 클릭된 마커의 이미지를 기본 이미지로 변경하고
+					!!selectedMarker && selectedMarker.setImage(normalImage);
+					// 현재 클릭된 마커의 이미지는 클릭 이미지로 변경합니다
+					marker.setImage(clickImage);
+				}
+				// 클릭된 마커를 현재 클릭된 마커 객체로 설정합니다
+				selectedMarker = marker;
 				clickedOverlay = customOverlay;
-				const address = place.road_address_name
-					? place.road_address_name + ' ' + place.place_name
-					: place.address_name + ' ' + place.place_name;
-				setY(place.y);
-				setX(place.x);
-				setAddress(address);
 			});
 		}
 	}, []);
@@ -146,9 +194,7 @@ const Map = ({ keyword }) => {
 	// 지도를 표시할 div와 지도 옵션으로 지도를 생성합니다
 	return (
 		<MapWrapper>
-			<div
-				id="map"
-				style={{ width: '100%', height: '40rem' }}></div>
+			<div id="map"></div>
 		</MapWrapper>
 	);
 };
