63a1a6f50c8fe1350447185c50fe423ec3023a1b
Dropper-in <dropperin59@gmail.com>
2023-05-21 14:22:07 +0900

    feat : #149 내 지역 게시글 데이터 받아오기 실패 시 메인 페이지로 이동

--- project_frontend/src/pages/myRegionBoardsMap.js
+++ project_frontend/src/pages/myRegionBoardsMap.js
@@ -1,45 +1,47 @@
 import { useEffect, useState } from 'react';
-import { WriteWrapper, MyRegionMap } from '../components';
+import { MyRegionMap } from '../components';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 function MyRegionBoardsMap() {
+	const navigate = useNavigate();
 	const [isLoaded, setIsLoaded] = useState(false);
 	const [places, setPlaces] = useState([]);
 	const getRecruitments = () => {
 		axios
 			.get('/api/boards/recruitment/places?page=0&size=30&sort=id')
 			.then((res) => {
-				const recruitments = res.data;
+				const recruitments = res.data.boardPlaceMarkerResponses;
 				setPlaces(recruitments);
+				setIsLoaded(true);
 			})
 			.catch((error) => {
 				console.log(error);
 				alert('데이터 받아오기에 실패했습니다.');
-				setIsLoaded(true);
-				setPlaces([
-					{
-						'recruitmentBoardIdResponse': {
-							'boardId': 46,
-						},
-						'meetingPlaceResponse': {
-							'longitude': 126.958177114993,
-							'latitude': 37.4960919285211,
-							'meetingAddress': '서울 동작구 상도로 369 숭실대학교 우편취급국',
-						},
-						'title': 'useEffect 실행후!',
-					},
-					{
-						'recruitmentBoardIdResponse': {
-							'boardId': 47,
-						},
-						'meetingPlaceResponse': {
-							'longitude': 126.955595261123,
-							'latitude': 37.4963495751839,
-							'meetingAddress': '서울 동작구 상도로 369 숭실대학교 주차장',
-						},
-						'title': 'useEffect 실행후!!!',
-					},
-				]);
-				setIsLoaded(true);
+				navigate('/');
+				// setPlaces([
+				// 	{
+				// 		'recruitmentBoardIdResponse': {
+				// 			'boardId': 46,
+				// 		},
+				// 		'meetingPlaceResponse': {
+				// 			'longitude': 126.958177114993,
+				// 			'latitude': 37.4960919285211,
+				// 			'meetingAddress': '서울 동작구 상도로 369 숭실대학교 우편취급국',
+				// 		},
+				// 		'title': 'useEffect 실행후!',
+				// 	},
+				// 	{
+				// 		'recruitmentBoardIdResponse': {
+				// 			'boardId': 47,
+				// 		},
+				// 		'meetingPlaceResponse': {
+				// 			'longitude': 126.955595261123,
+				// 			'latitude': 37.4963495751839,
+				// 			'meetingAddress': '서울 동작구 상도로 369 숭실대학교 주차장',
+				// 		},
+				// 		'title': 'useEffect 실행후!!!',
+				// 	},
+				// ]);
 			});
 	};
 	useEffect(() => {
