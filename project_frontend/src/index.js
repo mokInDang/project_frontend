38edaf5628b3783e296b4c8bfba10c07091dbbba
Dropper-in <dropperin59@gmail.com>
2023-05-25 02:53:01 +0900

    test : #174 ScrollToTop 컴포넌트 없이도 페이지 이동시 최상단 스크롤하는지 테스트

--- project_frontend/src/index.js
+++ project_frontend/src/index.js
@@ -14,7 +14,6 @@ axios.defaults.withCredentials = true;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<BrowserRouter>
-		<ScrollToTop />
 		<App />
 	</BrowserRouter>
 );
