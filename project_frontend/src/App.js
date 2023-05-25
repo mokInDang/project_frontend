73e8c69ae9cbd5470e3bb847cc9bc21f52b94e8d
Dropper-in <dropperin59@gmail.com>
2023-05-25 22:47:18 +0900

    refactor : #191  App.js에서 서브 페이지(무한 스크롤 컴포넌트) 라우팅

--- project_frontend/src/App.js
+++ project_frontend/src/App.js
@@ -18,6 +18,12 @@ import { Navigator, TopButton } from './components';
 import { reissueToken } from './apis';
 import { Comments } from './pages/comments';
 import { PrivateRoutes, RegionRequiredRoutes } from './utils';
+import {
+	RecruitmentInfiniteScroll,
+	CertificationCardInfiniteScroll,
+	InfiniteScroll,
+	MyRegionInfiniteScroll,
+} from './components';
 function App() {
 	// 페이지 리로드 시 reissueToken 실행
 	console.log('App.js에서 reissueToken 실행');
@@ -29,7 +35,26 @@ function App() {
 			<TopButton />
 			<Routes>
 				{/* 인증 여부 상관 없이 접근 가능한 페이지 정의 */}
-				<Route exact path='/*' element={<Home />} />
+				<Route path='mypage' element={<MyPage />} />
+				<Route path='mypage/edit' element={<MyInfoEdit />} />
+
+				<Route element={<Home />}>
+					<Route element={<RecruitmentInfiniteScroll />}>
+						<Route path='' element={<InfiniteScroll />} />
+						{/* <Route element={<PrivateRoutes authentication={true} />}>
+							<Route element={<RegionRequiredRoutes />}> */}
+						<Route
+							path='recruitment/myregion'
+							element={<MyRegionInfiniteScroll />}
+						/>
+						{/* </Route>
+						</Route> */}
+					</Route>
+					<Route
+						path='/certification'
+						element={<CertificationCardInfiniteScroll />}
+					/>
+				</Route>
 
 				<Route element={<PrivateRoutes authentication={false} />}>
 					{/* 인증을 반드시 하지 않아야만 접근 가능한 페이지 정의 */}
@@ -49,14 +74,6 @@ function App() {
 						path='/boards/certification/:boardId'
 						element={<CertificationDetails />}
 					/>
-					<Route element={<RegionRequiredRoutes />}>
-						<Route path='/boards/recruitment' element={<PostRecruitment />} />
-						<Route
-							path='/boards/certification'
-							element={<PostCertification />}
-						/>
-						<Route path='/myregionmap' element={<MyRegionBoardsMap />} />
-					</Route>
 					<Route
 						path='/edit/recruitment/:boardId'
 						element={<PatchRecruitment />}
@@ -65,6 +82,14 @@ function App() {
 						path='/edit/certification/:boardId'
 						element={<PatchCertification />}
 					/>
+					<Route element={<RegionRequiredRoutes />}>
+						<Route path='/boards/recruitment' element={<PostRecruitment />} />
+						<Route
+							path='/boards/certification'
+							element={<PostCertification />}
+						/>
+						<Route path='/myregionmap' element={<MyRegionBoardsMap />} />
+					</Route>
 				</Route>
 				{/* Comments도 추후 PrivateRoutes 안으로 옮길 것 */}
 				<Route
@@ -75,6 +100,7 @@ function App() {
 					path='/boards/recruitment/:boardId/comments'
 					element={<Comments />}
 				/>
+				<Route path='*' element={<Welcome />} />
 			</Routes>
 		</div>
 	);
