53a2052913b297a35dc18bccb5fe4474791c6f6b
Dropper-in <dropperin59@gmail.com>
2023-08-02 12:30:00 +0900

    feat : #192 서버에서 위젯 데이터 받아오기

--- project_frontend/src/components/main/recruitmentInfiniteScroll.js
+++ project_frontend/src/components/main/recruitmentInfiniteScroll.js
@@ -3,21 +3,24 @@ import { TabWrapper, TabDiv } from '../../components';
 import { useNavigate, useLocation, Outlet } from 'react-router';
 import { fireEmoji } from '../../assets/images';
 import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const WidgetDiv = styled.div`
 	position: relative;
 	font-weight: 700;
 	line-height: 1.3;
+	font-family: NanumSquareNeo;
 	.titleWrap {
 		img {
-			height: 1.8rem;
+			height: 2rem;
 			margin-right: 1rem;
 		}
 		div {
 			flex-shrink: 0;
 		}
 		display: inline-flex;
-		font-size: 1.8rem;
+		font-size: 2rem;
 		background: white;
 		padding-right: 1rem;
 		padding-bottom: 1rem;
@@ -36,7 +39,11 @@ const WidgetDiv = styled.div`
 		padding: 2rem 3.5rem;
 		background: rgba(245, 245, 245);
 		border-radius: 2.5rem;
-		font-size: 1.5rem;
+		font-family: NanumSquare;
+		font-weight: 900;
+		font-size: 1.7rem;
+		display: flex;
+		flex-direction: column;
 		margin: 0;
 		margin-top: 0.5rem;
 		line-height: 2;
@@ -48,40 +55,61 @@ const WidgetDiv = styled.div`
 			}
 			.regionName {
 				flex-shrink: 0;
-				text-decoration: underline;
+				text-decoration: underline wavy rgba(129, 204, 85, 0.5);
 				text-underline-position: under;
 			}
 			:hover {
 				.regionName {
-					text-decoration: underline wavy rgba(129, 204, 85, 0.5);
+					text-decoration: underline dotted rgba(129, 204, 85, 0.5);
 				}
 			}
 		}
 	}
 `;
 const Widget = () => {
-	const list = ['동작구', '관악구', '용산구', '서초구', '영등포구'];
+	const [list, setList] = useState([
+		'동작구',
+		'관악구',
+		'용산구',
+		'서초구',
+		'영등포구',
+	]);
+	const getRegionRank = () => {
+		axios
+			.get('/api/boards/recruitment/region-rank?page=0&size=5')
+			.then((res) => {
+				const data = res.data;
+				// console.log(data);
+				setList(res.data.regions);
+			})
+			.catch((error) => console.log(error));
+	};
+	useEffect(() => {
+		getRegionRank();
+	}, []);
+	useEffect(() => {
+		// console.log(list);
+	}, [list]);
 	return (
 		<div className='widget'>
 			<WidgetDiv>
-				<div className='connectLine'></div>
+				{/* <div className='connectLine'></div> */}
 				<div className='titleWrap'>
 					<img src={fireEmoji} />
-					<div>
-						현재 플로깅이
-						<br />
-						HOT한 지역
-					</div>
+					<div>지역별 플로깅 랭킹</div>
 				</div>
 				<ul className='regionList'>
-					{list.map(function (element, i) {
-						return (
-							<li key={i}>
-								<div className='regionIndex'>{i + 1}.</div>
-								<div className='regionName'>{element}</div>
-							</li>
-						);
-					})}
+					<div>
+						{list &&
+							list.map(function (element, i) {
+								return (
+									<li key={i}>
+										<div className='regionIndex'>{i + 1}.</div>
+										<div className='regionName'>{element.region}</div>
+									</li>
+								);
+							})}
+					</div>
 				</ul>
 			</WidgetDiv>
 		</div>
@@ -99,20 +127,14 @@ const RecruitmentInfiniteScroll = () => {
 
 	return (
 		<>
-			<div className='widgetWrapper'>
-				<div className='widgetSection'>
-					<div className='widgetDiv'>
-						<Widget></Widget>
-					</div>
-				</div>
-			</div>
 			<TabWrapper regionTab={regionTab}>
 				<TabDiv
 					onClick={(e) => {
 						// e.preventDefault();
 						navigate('/');
 					}}
-					className='regionTab entireRegion'>
+					className='regionTab entireRegion'
+				>
 					<ImFileEmpty size='2.5rem' style={{ marginRight: '1rem' }} />
 					전체
 				</TabDiv>
@@ -122,11 +144,19 @@ const RecruitmentInfiniteScroll = () => {
 						navigate('/recruitment/myregion');
 						// isLogined(setRegionTab, 'myRegion', navigate);
 					}}
-					className='regionTab myRegion'>
+					className='regionTab myRegion'
+				>
 					<ImFileText2 size='2.5rem' style={{ marginRight: '1rem' }} />내 지역
 				</TabDiv>
 			</TabWrapper>
 			<div className='infiniteScrollWrap'>
+				<div className='widgetWrapper'>
+					<div className='widgetSection'>
+						<div className='widgetDiv'>
+							<Widget></Widget>
+						</div>
+					</div>
+				</div>
 				<Outlet />
 			</div>
 		</>
